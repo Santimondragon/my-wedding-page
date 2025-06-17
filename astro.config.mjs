176199cbc7e92b6930a89d95a80328726398f45c
mg-smondragon <santimondragonm@gmail.com>
2025-06-17 00:07:30 -0500

    Added Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'http://auraysantisecasan.com/',
     base: '/',
     output: 'server',
+    adapter: vercel({}),
     vite: {
         envPrefix: 'PUBLIC_'
     }
