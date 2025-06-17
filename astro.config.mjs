0058c98ef7d6911d2f7aac264783cfb54e0ed502
mg-smondragon <santimondragonm@gmail.com>
2025-06-16 23:20:14 -0500

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    vite: {
-        server: {
-            headers: {
-                'Set-Cookie': 'SameSite=Strict; Secure'
-            }
-        }
-    }
+    site: 'http://auraysantisecasan.com/',
+    base: '/my-wedding-page',
+    output: 'static'
 });
